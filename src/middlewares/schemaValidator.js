c6007b5c6e4b698fc93f880cc460302335ca92d9
pipeboxes <felipe.cajas.maureira@gmail.com>
2025-02-17 15:14:01 -0300

    Modificaciones middleware

--- src/middlewares/schemaValidator.js
+++ src/middlewares/schemaValidator.js
@@ -12,7 +12,7 @@ const loginSchema = Joi.object({
 });
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers['authorization'];
+  const token = req.headers['authorization']?.split(' ')[1];;
 
   if (!token) {
     return res.status(403).json({ error: "Token no proporcionado" });
