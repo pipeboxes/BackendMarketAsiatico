c1d776d16be97cbf6fa8421a5a1f94cb5a22ce5e
pipeboxes <felipe.cajas.maureira@gmail.com>
2025-03-03 23:31:11 -0300

    Modificaciones controllers/user.js ruta email

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -152,10 +152,10 @@ const sendContactEmail = async (req, res) => {
     );
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      service: 'smtp.gmail.com',
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        pass: process.env.EMAIL_PASS
       },
     });
 
---
+++ tests/testemail.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: 'tu_correo@gmail.com',
+    subject: 'Prueba de correo',
+    text: 'Este es un correo de prueba desde Node.js'
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.error('Error al enviar el correo:', error);
+    } else {
+        console.log('Correo enviado:', info.response);
+    }
+});
